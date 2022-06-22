39a4892f525b71fb40cd2c2e72b41f3d034673fe
mfsaglam <mfsaglam@yandex.com>
2022-06-23 00:49:13 +0300

    add sinkExampleManual

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,24 @@
 import Combine
 
+/**
+Subscribers
 
+Sink
+Assign
+ 
+**/
+
+func sinkExampleManual() {
+    let publisher = [1, 3, 5, 8, 11].publisher
+    
+    let subscriber = Subscribers.Sink<Int, Never> { completion in
+        print(completion)
+    } receiveValue: { value in
+        print(value)
+    }
+    
+    publisher.subscribe(subscriber)
+    
+}
+
+sinkExampleManual()
