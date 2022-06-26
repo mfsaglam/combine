a0d4b7df68cb59a0c654578cf4c706dc8722e34c
mfsaglam <mfsaglam@yandex.com>
2022-06-26 23:10:15 +0300

    add scan

--- Basic Operators.playground/Contents.swift
+++ Basic Operators.playground/Contents.swift
@@ -18,3 +18,16 @@ example("Filter") {
         .filter { $0 < 60 }
         .sink { print ($0) }
 }
+
+example("Scan") {
+    let grades: [Double] = [98, 65, 49, 99, 100, 95]
+    _ = grades.publisher
+        .scan((avg: 0.0, sum: 0.0, count: 0.0)) { tuple, grade in
+            let newSum = tuple.sum + grade
+            let count = tuple.count + 1
+            let avg = newSum / Double(count)
+            return(avg: avg, sum: newSum, count: count)
+        }
+        .map { $0.avg }
+        .sink { print($0) }
+}
