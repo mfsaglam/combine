31225e2c8d2b84b29e7adf499094265eed9d932a
mfsaglam <mfsaglam@yandex.com>
2022-06-27 22:10:37 +0300

    merge

--- Operators-multipleStreams.playground/Contents.swift
+++ Operators-multipleStreams.playground/Contents.swift
@@ -6,4 +6,11 @@ func example(_ title: String, _ block: () -> Void ) {
     print("----------------------------\n\n")
 }
 
-
+example("merge") {
+    let pub1 = CurrentValueSubject<Int, Never>(100)
+    let pub2 = [1, 2, 3].publisher
+    
+    let merged = pub1.merge(with: pub2)
+    
+    merged.sink { print($0) }
+}
