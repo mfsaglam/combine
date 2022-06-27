325996084b0231adaac79633f76668dd321c09ff
mfsaglam <mfsaglam@yandex.com>
2022-06-27 22:22:57 +0300

    merge multiple

--- Operators-multipleStreams.playground/Contents.swift
+++ Operators-multipleStreams.playground/Contents.swift
@@ -11,6 +11,18 @@ example("merge") {
     let pub2 = [1, 2, 3].publisher
     
     let merged = pub1.merge(with: pub2)
-    
+        .print()
     merged.sink { print($0) }
+    
+    pub1.send(200)
+    pub1.send(completion: .finished)
+}
+
+example("merge multiple") {
+    let pub1 = [1, 2, 3].publisher
+    let pub2 = [4, 5, 6].publisher
+    let pub3 = [7, 8, 9].publisher
+    
+    Publishers.MergeMany([pub1, pub2, pub3])
+        .sink{ print($0) }
 }
