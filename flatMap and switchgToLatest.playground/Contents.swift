10cbd427d8bc6cbe24d836c2539a3cdae50772bd
mfsaglam <mfsaglam@yandex.com>
2022-06-28 21:53:41 +0300

    initial code

--- flatMap and switchgToLatest.playground/Contents.swift
+++ flatMap and switchgToLatest.playground/Contents.swift
@@ -1,2 +1,15 @@
+import Foundation
 import Combine
+import PlaygroundSupport
 
+//flatMap and switchToLatest
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let urls = [
+    "https://combineswift.com",
+    "https://apple.com",
+    "https://twitter.com"
+].map { URL(string: $0)! }
+
+var cancellables: Set<AnyCancellable> = []
