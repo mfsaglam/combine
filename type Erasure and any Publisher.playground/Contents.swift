7435545bd4ba9c0b272318e673822301de710470
mfsaglam <mfsaglam@yandex.com>
2022-07-01 23:00:23 +0300

    AnyPublisher example

--- type Erasure and any Publisher.playground/Contents.swift
+++ type Erasure and any Publisher.playground/Contents.swift
@@ -1,4 +1,14 @@
 import Foundation
 import Combine
 
+func myNumberPublisher() -> AnyPublisher<Int, Never> {
+    let publisher = [1, 2, 3].publisher
+        .merge(with: [4, 5, 6].publisher)
+        .dropFirst()
+        .prepend([1, 2, 3])
+    
+    return publisher
+        .eraseToAnyPublisher()
+}
 
+myNumberPublisher()
