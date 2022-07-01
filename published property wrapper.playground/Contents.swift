770247568cabfd109a5744a9bf91588cfc0dc4c8
mfsaglam <mfsaglam@yandex.com>
2022-07-01 23:19:05 +0300

    Publisher property wrapper example

--- published property wrapper.playground/Contents.swift
+++ published property wrapper.playground/Contents.swift
@@ -1,3 +1,27 @@
 import Foundation
 import Combine
 
+class Car {
+    @Published private(set) var gasLevel = 1.0
+    
+    func drive() {
+        if gasLevel > 0 {
+            print("🚗💨")
+            gasLevel -= 0.25
+        } else {
+            print("🔴 OUT OF GAS")
+        }
+    }
+}
+
+let car = Car()
+car.$gasLevel
+    .drop(while: { $0 > 0.4 })
+    .sink { print("WARNING❗️ LOW ON GAS \($0)") }
+
+car.drive()
+car.drive()
+car.drive()
+car.drive()
+car.drive()
+
