6c94725aaba68c1a6e1c58768789fc38e789c975
mfsaglam <mfsaglam@yandex.com>
2022-07-14 16:59:02 +0300

    throwing error from an operator

--- throwing errors from operators.playground/Contents.swift
+++ throwing errors from operators.playground/Contents.swift
@@ -1,4 +1,19 @@
 import Foundation
 import Combine
 
+struct DivideByZeroError: Error { }
+
+let denominators = [4, 3, 2, 7, 0].publisher
+
+denominators
+    .tryMap { value -> Double in
+        guard value != 0 else { throw DivideByZeroError() }
+        return 10.0 / Double(value)
+    }
+    .sink { completion in
+        print(completion)
+    } receiveValue: { value in
+        print(value)
+    }
+
 
