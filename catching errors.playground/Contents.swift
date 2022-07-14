39796942aab99641dd33ee5b45767692ee5d990f
mfsaglam <mfsaglam@yandex.com>
2022-07-14 14:13:28 +0300

    add subjects with failure type and sink to it

---
+++ catching errors.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+import Combine
+
+struct DummyError: Error { }
+
+let subject = PassthroughSubject<Int, DummyError>()
+
+subject.sink { completion in
+    print("completion: \(completion)")
+} receiveValue: { value in
+    print("value: \(value)")
+}
+
+subject.send(1)
+subject.send(completion: .failure(DummyError()))
+
