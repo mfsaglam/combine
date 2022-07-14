842731968edd2d7c2da7155563592d0b176d1555
mfsaglam <mfsaglam@yandex.com>
2022-07-14 14:29:26 +0300

    catch block

--- catching errors.playground/Contents.swift
+++ catching errors.playground/Contents.swift
@@ -15,3 +15,18 @@ subject.send(1)
 subject.send(completion: .failure(DummyError()))
 subject.send(2)
 
+let subject2 = PassthroughSubject<Int, DummyError>()
+
+subject2
+    .catch { error -> Just<Int> in
+        print("error \(error)")
+        return Just(-1)
+    }.sink { completion in
+        print("completion: \(completion)")
+    } receiveValue: { value in
+        print("value: \(value)")
+    }
+
+subject2.send(1)
+subject2.send(completion: .failure(DummyError()))
+
