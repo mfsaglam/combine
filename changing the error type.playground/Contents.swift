8dcd886a218d9a65ba1d80c732b2f70d609455a1
mfsaglam <mfsaglam@yandex.com>
2022-07-15 16:33:24 +0300

    failure type

--- changing the error type.playground/Contents.swift
+++ changing the error type.playground/Contents.swift
@@ -1,8 +1,19 @@
 import Foundation
 import Combine
 
+/*
+ Sometimes we want to combine 2 publishers together such as CombineLatest, that needs us to match the output and error types.
+ That case we need to set a failure type.
+*/
+
 let publisher = [1, 2, 3].publisher
+    .setFailureType(to: Error.self)
+let subject = PassthroughSubject<Int, Error>()
 
+enum FriendlyErrors: Error {
+    case unableToConnect
+    case other(URLError)
+}
 
-
+let networkPublisher = PassthroughSubject<String, FriendlyErrors>()
 
