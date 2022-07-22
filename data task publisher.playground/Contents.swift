9abb999e608c93b4826d7a18df28ce26cef59828
mfsaglam <mfsaglam@yandex.com>
2022-07-22 18:11:12 +0300

    network request made as soon as the publisher gets a subscription

--- data task publisher.playground/Contents.swift
+++ data task publisher.playground/Contents.swift
@@ -11,5 +11,19 @@ let session = URLSession.shared
 let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1")!
 
 func example1() {
-    
+    // Output = (data: Data, response: URLResponse)
+    // Failure = URLError
+    var cancellable: AnyCancellable?
+    cancellable = session.dataTaskPublisher(for: url)
+        .print()
+        .map { $0.data }
+        .replaceError(with: Data())
+        .map { String(data: $0, encoding: .utf8) }
+        .sink { response in
+            print("response \(response ?? "<no body>")")
+            cancellable = nil
+            _ = cancellable
+        }
 }
+
+example1()
