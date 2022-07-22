aff3d38195500e4b5b5bfc58a8b20229b444075f
mfsaglam <mfsaglam@yandex.com>
2022-07-22 18:42:04 +0300

    make a network call

--- data task publisher.playground/Contents.swift
+++ data task publisher.playground/Contents.swift
@@ -26,4 +26,23 @@ func example1() {
         }
 }
 
-example1()
+func fetchData() -> AnyPublisher<String?, Never> {
+    session.dataTaskPublisher(for: url)
+        .print()
+        .map { $0.data }
+        .replaceError(with: Data())
+        .map { String(data: $0, encoding: .utf8) }
+        .eraseToAnyPublisher()
+}
+
+print("getting publisher...")
+
+let publisher = fetchData()
+
+var cancellable: AnyCancellable?
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+    cancellable = publisher.sink { response in
+        print(response ?? "<no body>")
+    }
+}
