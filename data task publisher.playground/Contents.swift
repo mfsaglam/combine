029aa640adabe6f8b151027618160b0883da5fa6
mfsaglam <mfsaglam@yandex.com>
2022-07-22 16:50:40 +0300

    add url session, url, example method

--- data task publisher.playground/Contents.swift
+++ data task publisher.playground/Contents.swift
@@ -3,3 +3,13 @@ import Combine
 import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
+
+/// # Data Task Publishers
+
+let session = URLSession.shared
+
+let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1")!
+
+func example() {
+    
+}
